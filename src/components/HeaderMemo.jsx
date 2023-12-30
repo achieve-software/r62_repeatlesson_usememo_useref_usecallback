5eefd9762af440398b5027fe63fa2e51deccd0cb
Achieve-software <vtcbcdtyr@gmail.com>
2023-12-31 00:20:56 +0300

    memo usage

--- src/components/HeaderMemo.jsx
+++ src/components/HeaderMemo.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React,{memo} from "react";
 
-const HeaderMemo = () => {
-  return (
-    <div className='text-center text-success'>HeaderMemo Componenti</div>
-  )
-}
+//arrow func larda 1.kullanım
+// const HeaderMemo = memo(({user}) => {
+//   console.log("Render => HeaderMemo componenti")
+//   return <div className="text-center text-success">HeaderMemo Componenti : {user}</div>;
+// });
 
-export default HeaderMemo
+// export default HeaderMemo;
+
+// memo 2.kullanım
+const HeaderMemo = ({count}) => {
+  console.log("Render => HeaderMemo componenti")
+  return <div className="text-center text-success">HeaderMemo Componenti : {count}</div>;
+};
+
+export default memo(HeaderMemo);
